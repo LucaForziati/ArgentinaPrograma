b023ac583bc4d34f95dae3b9969de28a7e731656
Luca Forziati Gangi <lucaforziati@hotmail.com>
2022-07-26 22:32:53 -0300

    first commit backend

---
+++ backend/src/main/java/com/portfolio/luca/LucaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.luca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaApplication.class, args);
+	}
+
+}
